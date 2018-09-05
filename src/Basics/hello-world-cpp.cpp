6645f55f5e42e419338831498f891d83242e2782
Tharindu Amila <tharindu@emojot.com>
2018-09-05 23:57:16 +1000

    Mandatory Hello world in c and cpp.

---
+++ src/Basics/hello-world-cpp.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tharindu on 9/5/18.
+//
+#include <iostream>
+
+int main(int argc, char ** argv) {
+    std::cout << "Hello, World from CPP !!" << std::endl;
+    return 0;
+}
---
+++ src/Basics/hello-world.c
@@ -0,0 +1,9 @@
+//
+// Created by Tharindu on 9/5/18.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!!\n");
+    return 0;
+}
